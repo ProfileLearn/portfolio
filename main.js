abf4c803ae1cae32e699d99cf2320705c4d169c8
profilelearn <profilelearn@gmail.com>
2020-07-23 19:03:23 -0300

    agrego carpeta images

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const URLA = `https://profilelearn.github.io/`;
-const URLI = [`https://github.com/ProfileLearn/`, `/blob/master/images/image.png?raw=true`];
+const URLI = [`https://github.com/ProfileLearn/portfolio/blob/master/images/`, `${".png"||".jpg"}?raw=true`];
+
 
 class Datos {
 	constructor(hrefA, titulo) {
@@ -12,26 +13,30 @@ class Datos {
 
 
 function render(obj) {
-	const temp = document.createElement("template");
-	temp.innerHTML = `<a href = ${obj.hrefA} class="project-tile project" target="_blank" rel="noopener noreferrer">
-  	<img src = ${obj.srcImg} alt="project" class="project-image">
-  	<p class="project-title"><em>${obj.titulo}</em></p>
-	</a>`;
-	document.querySelector(".projects-grid").appendChild(temp.content);
-	console.dir(document.querySelector(".projects-grid"))
+	const a = document.createElement("a");
+	a.className = "project-tile project";
+	a.target="_blank";
+	a.rel="noopener noreferrer";
+	a.href = obj.hrefA;
+	a.innerHTML = `<img src = ${obj.srcImg} alt="project" class="project-image">
+  	<p class="project-title"><em>${obj.titulo}</em></p>`
+
+	document.querySelector(".projects-grid").appendChild(a);
 }
 
 const app = [];
 
 app[0] = new Datos("app_enfoque", "Ejercicio para el cerebro");
-/*app[1] = new Datos("interescompuestobs4", "Calculador de Intereses");
+app[1] = new Datos("interescompuestobs4", "Calculador de Intereses");
 app[2] = new Datos("calculator", "Calculadora");
 app[3] = new Datos("technical-documentation-page", "Página de documentacion Técnica");
 app[4] = new Datos("product-landing-page", "Landing Page");
 app[5] = new Datos("survey-form", "Survey Form");
-app[6] = new Datos("biografia", "Biografía");*/
+app[6] = new Datos("biografia", "Biografía");
 
-render(app[0])
+app.forEach(e=>{
+	render(e);
+})
 
 
 
