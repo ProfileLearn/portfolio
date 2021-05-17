e2c4b0fe2d045c5892baa44fefa400045777ce32
Profile Learn <profilelearn@gmail.com>
2021-05-17 18:41:11 -0300

    sndjkosd

--- main.js
+++ main.js
@@ -50,9 +50,9 @@ responsive();
 
 const app = [];
 
-app[0] = new Datos("app_enfoque", "Juego Mental");
+app[0] = new Datos("app_enfoque", "Juego Mental");|
 app[1] = new Datos("interescompuestobs4", "Calculador de Intereses");
-app[2] = new Datos("technical-documentation-page", "Página de documentacion Técnica");
+app[2] = new Datos("portrait-3", "Slider");
 
 app.forEach(e=>{
 	render(e);
